99e38289f428a802a4fce5e7b396dd5388d54d0f
AHMED-max-mustafa <aahhmm012315@gmail.com>
2025-01-06 01:09:53 +0200

    ww

---
+++ HOME WORK 6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+int rows =5;
+int cols =10;
+for(int i=1;i<=rows;i++){
+    for(int j=1;j<=cols;j++){
+        if(i==1||i==rows){
+            printf("#");}
+            else{
+                if(j==1||j==cols){
+                    printf("#");}
+                    else{
+                        printf("*");}
+            }
+    }
+    
+                        printf("\n");}
+    
+
+                        return(0);
+}
