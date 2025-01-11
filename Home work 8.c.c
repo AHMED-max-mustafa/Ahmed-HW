2ccf648e43230e47157f236b564daaeab783a8b1
AHMED-max-mustafa <aahhmm012315@gmail.com>
2025-01-11 20:57:42 +0200

    nn

---
+++ Home work 8.c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int x ;
+    int y;
+    printf("inter your number:\n");
+    scanf("%d",&x); 
+     scanf("%d",&y);
+     printf("%d",x+y);
+       return (0);   }
---
+++ home work 7.c.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+ int main(){
+char name[10];
+int x ;
+for (i=0;i<11;i++){
+if (name[i]>='0'&& name[i]<'11'){
+printf("inter your name :");
+ scanf("%S,",& name);}
+ else
+ if(i>11){
+ printf("inter your age:");
+ scanf("%d",&x);}
+ printf("your name is %s\n",name);
+ printf("your age is %d\n",x);}
+
+       return(0);}
